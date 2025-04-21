64d0bab9f154ea6df891842068d90a000b7ac70f
Derek Berger <derekberger734@gmail.com>
2025-04-20 21:46:54 -0400

    Update formatting on r53

--- terraform/r53.tf
+++ terraform/r53.tf
@@ -14,13 +14,13 @@ resource "aws_acm_certificate" "cert" {
 # DNS Validation Records #
 ##########################
 resource "aws_route53_record" "cert_validation" {
-  count            = 1
-  zone_id          = data.aws_route53_zone.hosted_zone.zone_id
-  name             = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
-  type             = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_type
-  ttl              = 60
-  records          = [ tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_value ]
-  allow_overwrite  = true
+  count           = 1
+  zone_id         = data.aws_route53_zone.hosted_zone.zone_id
+  name            = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
+  type            = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_type
+  ttl             = 60
+  records         = [tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_value]
+  allow_overwrite = true
 
   lifecycle {
     create_before_destroy = true
